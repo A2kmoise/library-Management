7f5abccd2dcff3462b041ccb420cf7a38f12b084
A2kmoise <abayomoise950@gmail.com>
2025-05-27 16:39:45 +0200

    second

--- include/member.h
+++ include/member.h
@@ -5,7 +5,7 @@ typedef struct {
     int member_id;
     char name[255];
     char email[255];
-    char date_joined[11]; // Format: YYYY-MM-DD
+    char date_joined[11]; 
 } Member;
 
 int add_member(Member member);
